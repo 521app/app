cc47fb01fe5be91fa571b48aaf12c7f83e5fd011
521app <18536667703@qq.com>
2024-08-18 17:55:06 +0800

    Add files via upload

---
+++ static/servers.js
@@ -0,0 +1,8 @@
+// 正式服地址
+window.windowUrl = {
+  api: "https://api.lumayun.co"
+}
+// 测试服地址
+// window.windowUrl = {
+//   api: "http://test.lumayun.vip"
+// }
---
+++ static/servers2.js
@@ -0,0 +1,8 @@
+// 正式服地址
+window.windowUrl = {
+  api: "https://api.liushanmen.xyz"
+}
+// 测试服地址
+// window.windowUrl = {
+//   api: "http://test.lumayun.vip"
+// }
